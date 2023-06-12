89c61ad3b898c4d2eeb8a8127b2902056edb63f4
Александр Гурьев <arh-tehnik@mail.ru>
2023-06-12 20:20:10 +0300

    Добавьте файлы проекта.

---
+++ fdjghalojhg/Human.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fdjghalojhg
+{
+    abstract class Human
+    {
+        public abstract string Firstname { get; set; }
+        public abstract string Lastname { get; set; }
+
+        public override string ToString()
+        {
+            return $" Фамилия {Lastname}, имя {Firstname}";
+        }
+    }
+    abstract class Employee : Human
+    {
+        public string Position { get; set; }
+        public string Salary { get; set; }
+        public override string ToString()
+        {
+            return base.ToString() + $" Должность {Position}, зарплата {Salary}";
+        }
+
+    }
+    interface IWoker
+    {
+        bool IsWorking { get; }
+        string Work();
+    }
+    interface IManager
+    {
+        List<IWoker> ListOfW { get; set; }
+        void oRG();
+        void Make();
+        void Control();
+    }
+    class Direktor : Employee, IManager
+    {
+
+       
+
+    public override string Firstname { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public override string Lastname { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<IWoker> ListOfW { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Control()
+    {
+        Console.WriteLine("Контроль работы ");
+    }
+
+    public void Make()
+    {
+        Console.WriteLine("Бюджет");
+    }
+
+    public void oRG()
+    {
+        Console.WriteLine("Работа");
+    }
+}
+
+
+
+
+
+
+
+}
+
+    
+    
+
+
+
---
+++ fdjghalojhg/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fdjghalojhg
+{
+    internal class Program
+    {
+        //либо публичный но чаще всего
+        public interface IResearcher
+        {
+            void Inverstigate();
+            void Invent();
+             string str { get; set; }
+        }
+        static void Main(string[] args)
+        {
+
+            Direktor direktor = new Direktor
+            {
+                Lastname = "Иванов",
+                Firstname = "Иванов  ",
+                Position = "Директор  ",
+                Salary = " 120"
+
+            };
+            Console.WriteLine($"Зарплата директора { (direktor.Lastname)},{direktor.Salary}");
+            
+
+        }
+    }
+}
